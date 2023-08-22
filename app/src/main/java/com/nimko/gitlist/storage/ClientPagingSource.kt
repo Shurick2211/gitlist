9085ac67d072d9941b032106e8feb78b56013047
Aleksandr Nimko <shurick2211@gmail.com>
2023-08-22 15:20:09 +0300

    PagingSources

---
+++ app/src/main/java/com/nimko/gitlist/storage/ClientPagingSource.kt
@@ -0,0 +1,35 @@
+package com.nimko.gitlist.storage
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.nimko.gitlist.dbservices.entitys.Client
+
+
+
+class ClientPagingSource(
+    val storage: Storage,
+    val perPage:Int
+):PagingSource<Int,Client>() {
+    override fun getRefreshKey(state: PagingState<Int, Client>): Int? {
+        val anchorPosition = state.anchorPosition ?: return null
+        val page = state.closestPageToPosition(anchorPosition) ?: return null
+        return page.prevKey?.plus(1) ?: page.nextKey?.minus(1)
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Client> {
+        val pageIndex = params.key ?: 0
+        return try {
+            val clients = storage.getClient(params.loadSize,pageIndex)
+            return LoadResult.Page(
+                data = clients,
+                prevKey = if(pageIndex == 0) null else pageIndex-1,
+                nextKey = if(params.loadSize == clients.size) pageIndex + (params.loadSize/perPage)
+                else null
+            )
+        } catch (e:Exception){
+            LoadResult.Error(
+                throwable = e
+            )
+        }
+    }
+}
